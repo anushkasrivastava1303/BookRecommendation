7bc7964fa18dc7d24d7ebffc71d7b89105917454
Arnav Shrivastava <anushkatia1303@gmail.com>
2023-11-19 00:42:25 +0530

    ADD v3.

--- fcc_book_recommendation_knn.py
+++ fcc_book_recommendation_knn.py
@@ -8,7 +8,7 @@ from scipy.sparse import csr_matrix
 from sklearn.neighbors import NearestNeighbors
 import matplotlib.pyplot as plt   
           
-# get data files      
+# get data files       
 !wget https://cdn.freecodecamp.org/project-data/books/book-crossings.zip
 !unzip book-crossings.zip 
         
